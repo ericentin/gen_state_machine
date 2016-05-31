962268904dfb4b6bf4e955aecf86bf06b206fa16
Eric Entin <eric@ericent.in>
2016-05-31 14:31:31 -0400

    add ex_doc and prepare mix.exs for publishing

--- mix.exs
+++ mix.exs
@@ -1,11 +1,42 @@
 defmodule GenStateMachine.Mixfile do
   use Mix.Project
 
+  @version "0.0.1"
+
   def project do
     [app: :gen_state_machine,
-     version: "0.0.1",
+     version: @version,
      elixir: "~> 1.3-rc",
      build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod]
+     start_permanent: Mix.env == :prod,
+     deps: deps,
+     description: description,
+     package: package,
+     docs: [
+       main: "GenStateMachine",
+       extras: ["README.md"],
+       source_ref: "v#{@version}"
+     ]]
+  end
+
+  defp deps do
+    [
+      {:ex_doc, "~> 0.11.5", only: :dev}
+    ]
+  end
+
+  defp description do
+    "An Elixir wrapper for gen_statem."
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Eric Entin"],
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => "https://github.com/antipax/gen_state_machine"
+      }
+    ]
   end
 end
