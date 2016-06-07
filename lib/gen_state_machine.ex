21821b5162ee9dbfa24643ac00b0843715a4178f
Eric Entin <eric@ericent.in>
2016-06-07 11:50:34 -0400

    Improve module doc

--- lib/gen_state_machine.ex
+++ lib/gen_state_machine.ex
@@ -88,6 +88,132 @@ defmodule GenStateMachine do
 
       GenStateMachine.call(pid, :get_count)
       #=> 1
+
+  Again, we start our `Switch` by calling `start_link/3`, passing the module
+  with the server implmentation and its initial argument, and interacting with
+  it via **call** and **cast**.
+
+  However, in `:state_functions` callback mode, every time you do a `call/3` or
+  a `cast/2`, the message will be handled by the `state_name/3` function which
+  is named the same as the current state.
+
+  ## Callbacks
+
+  In the default `:handle_event_function` callback mode, there are 4 callbacks
+  required to be implemented. By adding `use GenStateMachine` to your module,
+  Elixir will automatically define all 4 callbacks for you, leaving it up to you
+  to implement the ones you want to customize.
+
+  In the `:state_functions` callback mode, there are 3 callbacks required to be
+  implemented. By adding `use GenStateMachine, callback_mode: :state_functions`
+  to your module, Elixir will automatically define all 3 callbacks for you,
+  leaving it up to you to implement the ones you want to customize, as well as
+  `state_name/3` functions named the same as the states you wish to support.
+
+  ## Name Registration
+
+  Both `start_link/3` and `start/3` support registering the `GenStateMachine`
+  under a given name on start via the `:name` option. Registered names are also
+  automatically cleaned up on termination. The supported values are:
+
+    * an atom - the GenStateMachine is registered locally with the given name
+      using `Process.register/2`.
+
+    * `{:global, term}`- the GenStateMachine is registered globally with the
+      given term using the functions in the `:global` module.
+
+    * `{:via, module, term}` - the GenStateMachine is registered with the given
+      mechanism and name. The `:via` option expects a module that exports
+      `register_name/2`, `unregister_name/1`, `whereis_name/1` and `send/2`.
+      One such example is the `:global` module which uses these functions
+      for keeping the list of names of processes and their  associated pid's
+      that are available globally for a network of Erlang nodes.
+
+  For example, we could start and register our Switch server locally as follows:
+
+      # Start the server and register it locally with name MySwitch
+      {:ok, _} = GenStateMachine.start_link(Switch, {:off, 0}, name: MySwitch)
+
+      # Now messages can be sent directly to MySwitch
+      GenStateMachine.call(MySwitch, :get_count) #=> 0
+
+  Once the server is started, the remaining functions in this module (`call/3`,
+  `cast/2`, and friends) will also accept an atom, or any `:global` or `:via`
+  tuples. In general, the following formats are supported:
+
+    * a `pid`
+    * an `atom` if the server is locally registered
+    * `{atom, node}` if the server is locally registered at another node
+    * `{:global, term}` if the server is globally registered
+    * `{:via, module, name}` if the server is registered through an alternative
+      registry
+
+  ## Client / Server APIs
+
+  Although in the example above we have used `GenStateMachine.start_link/3` and
+  friends to directly start and communicate with the server, most of the
+  time we don't call the `GenStateMachine` functions directly. Instead, we wrap
+  the calls in new functions representing the public API of the server.
+
+  Here is a better implementation of our Switch module:
+
+      defmodule Switch do
+        use GenStateMachine
+
+        # Client
+
+        def start_link() do
+          GenStateMachine.start_link(Switch, {:off, 0})
+        end
+
+        def flip(pid) do
+          GenStateMachine.cast(pid, :flip)
+        end
+
+        def get_count(pid) do
+          GenStateMachine.call(pid, :get_count)
+        end
+
+        # Server (callbacks)
+
+        def handle_event(:cast, :flip, :off, data) do
+          {:next_state, :on, data + 1}
+        end
+
+        def handle_event(:cast, :flip, :on, data) do
+          {:next_state, :off, data}
+        end
+
+        def handle_event({:call, from}, :get_count, state, data) do
+          {:next_state, state, data, [{:reply, from, data}]}
+        end
+
+        def handle_event(event_type, event_content, state, data) do
+          # Call the default implementation from GenStateMachine
+          super(event_type, event_content, state, data)
+        end
+      end
+
+  In practice, it is common to have both server and client functions in
+  the same module. If the server and/or client implementations are growing
+  complex, you may want to have them in different modules.
+
+  ## Receiving custom messages
+
+  The goal of a `GenStateMachine` is to abstract the "receive" loop for
+  developers, automatically handling system messages, support code change,
+  synchronous calls and more. Therefore, you should never call your own
+  "receive" inside the GenServer callbacks as doing so will cause the GenServer
+  to misbehave. If you want to receive custom messages, they will be delivered
+  to the usual handler for your callback mode with event_type `:info`.
+
+  ## Learn more
+
+  If you wish to find out more about gen_statem, the documentation and links
+  in Erlang can provide extra insight.
+
+    * [`:gen_statem` module documentation](http://erlang.org/documentation/doc-8.0-rc1/lib/stdlib-3.0/doc/html/gen_statem.html)
+    * [gen_statem Behaviour – OTP Design Principles](http://erlang.org/documentation/doc-8.0-rc1/doc/design_principles/statem.html)
   """
 
   @type state :: state_name | term
@@ -220,6 +346,7 @@ defmodule GenStateMachine do
     quote location: :keep do
       defoverridable [init: 1, code_change: 4]
 
+      @doc false
       def init(args) do
         case super(args) do
           {:ok, state, data} -> {@gen_statem_callback_mode, state, data}
@@ -228,6 +355,7 @@ defmodule GenStateMachine do
         end
       end
 
+      @doc false
       def code_change(old_vsn, state, data, extra) do
         case super(old_vsn, state, data, extra) do
           {:handle_event_function, state, data} -> {:handle_event_function, state, data}
